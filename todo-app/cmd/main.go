0952d68dded6938e044137cd9e6b36ee1693ccf5
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-12-01 20:13:59 +0300

    Add files via upload

--- todo-app/cmd/main.go
+++ todo-app/cmd/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"github.com/geges1101/todo-app"
+	"github.com/geges1101/todo-app/pkg/handler"
+	"github.com/geges1101/todo-app/pkg/repository"
+	"github.com/geges1101/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
